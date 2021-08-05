9c3a8c6395406647f6f5b6c4bdab01f52ef07265
Amado Azua III <amado.azua3@gmail.com>
2021-08-05 17:02:20 -0500

    movies-backend

--- src/main/java/MovieServlet.java
+++ src/main/java/MovieServlet.java
@@ -1,31 +1,157 @@
 import com.google.gson.Gson;
+import data.DaoFactory;
 import data.Movie;
+import data.MoviesDao;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
 
 @WebServlet(name="MovieServlet", urlPatterns = "/movies")
 public class MovieServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response){
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
 
         response.setContentType("application/json");
 
+        PrintWriter out = null;
+
         try {
-            PrintWriter out = response.getWriter();
-            Movie movie = new Movie(2, "King Kong", "1942", "Harry Carey", "Fay Ray", "8675309", "there aint one", "cheap", "gorilla on skyscrapper");
 
-            String movieString = new Gson().toJson(movie);
+            out = response.getWriter();
 
-            out.println(movieString);
+//            Movie movie = new Movie(2, "King Kong", "1942", "Harry Carey", "Fay Ray", "8675309", "there aint one", "cheap", "gorilla on skyscrapper");
+
+            MoviesDao moviesDao = DaoFactory.getMoviesDao(DaoFactory.ImplType.IN_MEMORY);
+
+            String moviesString = new Gson().toJson(moviesDao.all());
+
+            out.println(moviesString);
 
         } catch(Exception ex){
             System.out.println(ex.getMessage());
         }
     }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+
+        response.setContentType("application/json");
+
+        PrintWriter out = null;
+
+        try{
+
+            out = response.getWriter();
+
+            BufferedReader reader = request.getReader();
+
+            Movie[] movies = new Gson().fromJson(reader, Movie[].class);
+
+            for(Movie movie: movies){
+                System.out.println(movie.getId());
+                System.out.println(movie.getTitle());
+                System.out.println(movie.getDirector());
+                System.out.println(movie.getActors());
+                System.out.println(movie.getGenre());
+                System.out.println(movie.getImdbID());
+                System.out.println(movie.getPlot());
+                System.out.println(movie.getPoster());
+                System.out.println("*****************************************");
+            }
+
+            out.println(new Gson().toJson(DaoFactory.getMoviesDao(DaoFactory.ImplType.IN_MEMORY).all()));
+
+        }catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+
+        out.println(new Gson().toJson("{message: \"Movies POST was successful\""));
+        response.setStatus(200);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response){
+
+        response.setContentType("application/json");
+
+        PrintWriter out = null;
+
+        try{
+
+            out = response.getWriter();
+
+            BufferedReader reader = request.getReader();
+
+//            Movie[] movies = new Gson().fromJson(reader, Movie[].class);
+
+            Movie movies = new Gson().fromJson(request.getReader(), Movie.class);
+            DaoFactory.getMoviesDao(DaoFactory.ImplType.IN_MEMORY).update(movies);
+
+            for(Movie movie: movies){
+                System.out.println(movie.getId());
+                System.out.println(movie.getTitle());
+                System.out.println(movie.getDirector());
+                System.out.println(movie.getActors());
+                System.out.println(movie.getGenre());
+                System.out.println(movie.getImdbID());
+                System.out.println(movie.getPlot());
+                System.out.println(movie.getPoster());
+                System.out.println("*****************************************");
+            }
+
+
+        } catch (SQLException e) {
+            out.println(new Gson().toJson(e.getLocalizedMessage()));
+            response.setStatus(500);
+            e.printStackTrace();
+            return;
+        } catch (Exception e) {
+            out.println(new Gson().toJson(e.getLocalizedMessage()));
+            response.setStatus(400);
+            e.printStackTrace();
+            return;
+        }
+
+        out.println(new Gson().toJson("{message: \"Movie UPDATE was successful\"}"));
+        response.setStatus(200);
+    }
+
+
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response){
+
+        response.setContentType("application/json");
+
+        PrintWriter out = null;
+
+        try{
+
+            out = response.getWriter();
+
+            BufferedReader reader = request.getReader();
+
+            Movie[] movies = new Gson().fromJson(reader, Movie[].class);
+
+            int id = new Gson().fromJson(reader, int.class);
+
+            System.out.println("The movie id to delete: " + id);
+
+
+
+        }catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+
+        out.println(new Gson().toJson("{message: \"Movies DELETE was successful\""));
+        response.setStatus(200);
+
+    }
+
 
 }
---
+++ src/main/java/data/DaoFactory.java
@@ -0,0 +1,24 @@
+package data;
+
+
+
+
+
+public class DaoFactory {
+
+    private static MoviesDao moviesDao;
+//    private static Config config = new Config();
+    public enum ImplType {MYSQL, IN_MEMORY}; // notice we have two values here
+
+    public static MoviesDao getMoviesDao(ImplType implementationType){
+
+        switch(implementationType){
+            case IN_MEMORY: { //yet we have one switch case. We'll get to that!
+                moviesDao = new InMemoryMoviesDao();
+            }
+        }
+        return moviesDao;
+    }
+
+
+}
